10384f267f0d16647a4da798462343b428536654
Gaurav Tandon <gauravt.4u@gmail.com>
2015-11-09 12:00:39 +1300

    changes to commit

--- lib/lita/handlers/ambush.rb
+++ lib/lita/handlers/ambush.rb
@@ -5,17 +5,33 @@ module Lita
     class Ambush < Handler
       # insert handler code here
 
-      route(/^ambush:\s+(\S+):\s*(.+)/, :test)
+      route(/^ambush:\s+(\S+):\s*(.+)/, :test, command: true)
+      route(/./, :reading)
 
       def test(response)
         store_hash = {
           time: Time.now.to_i,
-          msg: response.matches[0][1]
+          msg: response.matches[0][1],
+          ambusher: response.user.name
         }.to_yaml
 
         redis.lpush(response.matches[0][0], store_hash)
         # binding.pry
         response.reply("#{response.user.name} I've stored the ambush")
+      end
+
+      def reading(response)
+        # binding.pry
+        unless response.message.body.start_with? "ambush"
+          stored_yaml = redis.rpop(response.user.name)
+          while not stored_yaml.nil? do
+            outputted_yaml = YAML.load(stored_yaml)
+            response.reply("#{response.user.name}: While you were out, #{outputted_yaml[:ambusher]} said: #{outputted_yaml[:msg]}")
+            stored_yaml = redis.rpop(response.user.name)
+          end
+          
+        end
+
       end
 
 
--- spec/lita/handlers/ambush_spec.rb
+++ spec/lita/handlers/ambush_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require 'yaml'
 
 describe Lita::Handlers::Ambush, lita_handler: true do
 
@@ -9,25 +10,61 @@ describe Lita::Handlers::Ambush, lita_handler: true do
 
   it "testing storage into redis" do
     user1 = Lita::User.create(123, name: "user1")
-    send_message("ambush: user1: this is test plugin", as: user1)
+    send_command("ambush: user1: this is test plugin", as: user1)
     # require 'pry'; binding.pry
     expect(replies.last).to eq("#{user1.name} I've stored the ambush")
     redis_rpop= Lita.redis.rpop("handlers:ambush:#{user1.name}") ;
-    expect(redis_rpop ).to eq("this is test plugin")
+    outputed_yaml = YAML.load(redis_rpop)
+    expect(outputed_yaml[:time] ).to be >0
+    expect(outputed_yaml[:msg] ).to eq("this is test plugin")
   end
 
   it "should send out two messages" do
     user1 = Lita::User.create(123, name: "user1")
-    send_message("ambush: user1: this is test plugin data 1", as: user1)
-    send_message("ambush: user1: this is test plugin data 2", as: user1)
+    send_command("ambush: user1: this is test plugin data 1", as: user1)
+    send_command("ambush: user1: this is test plugin data 2", as: user1)
     expect(replies.last).to eq("#{user1.name} I've stored the ambush")
     expect(replies.last).to eq("#{user1.name} I've stored the ambush")
-    expect(Lita.redis.rpop("handlers:ambush:#{user1.name}") ).to eq("this is test plugin data 1")
-    expect(Lita.redis.rpop("handlers:ambush:#{user1.name}") ).to eq("this is test plugin data 2")
-
-
-
+    redis_rpop1 = Lita.redis.rpop("handlers:ambush:#{user1.name}")
+    redis_yaml1 = YAML.load(redis_rpop1)
+    expect( redis_yaml1[:msg]).to eq("this is test plugin data 1")
+    redis_rpop2 = Lita.redis.rpop("handlers:ambush:#{user1.name}")
+    redis_yaml2 = YAML.load(redis_rpop2)
+    expect( redis_yaml2[:msg]).to eq("this is test plugin data 2")
 
   end
+
+  it { is_expected.to route("...............") }
+  it { is_expected.to route("I am making good stuff") }
+  it { is_expected.to route("This is remarkable") }
+  it { is_expected.to route("Don't judge the book by its front cover") }
+
+  it "should respond with the message stored" do
+    user1 = Lita::User.create(123, name: "user1")
+    user2 = Lita::User.create(453, name: "user2")
+    send_command("ambush: #{user2.name}: this is a taco!", as: user1)
+    send_message("I like to watch a nice sci-fi movie", as: user2)
+    expect(replies.last).to eq("#{user2.name}: While you were out, #{user1.name} said: this is a taco!")
+  end
+
+  it "should do nothing with no messages" do
+    user1 = Lita::User.create(123, name: "user1")
+    user2 = Lita::User.create(453, name: "user2")
+    send_message("I like to watch a nice sci-fi movie", as: user2)
+    expect(replies.last).not_to eq("#{user2.name}: While you were out, #{user1.name} said: this is a taco!")
+  end
+
+  it "should respond with multiple messages stored" do
+    user1 = Lita::User.create(123, name: "user1")
+    user2 = Lita::User.create(453, name: "user2")
+    send_command("ambush: user2: this is test plugin data 1", as: user1)
+    send_command("ambush: user2: this is test plugin data 2", as: user1)
+    send_message("I like to watch a nice sci-fi movie", as: user2)
+    expect(replies[2]).to eq("#{user2.name}: While you were out, #{user1.name} said: this is test plugin data 1")
+    expect(replies[3]).to eq("#{user2.name}: While you were out, #{user1.name} said: this is test plugin data 2")
+
+  end
+  
+  
 
 end
